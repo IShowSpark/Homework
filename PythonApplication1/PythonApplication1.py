89913768add2631ae8f946e530d2012a1e680ec8
opilane <opilane@HP-CZC6297BB7>
2021-11-11 16:14:48 +0200

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,87 @@
+#1 zadanie
+#t=0 #кол-во чисел
+#q=0 #кол-во int чисел
+#while t<15:
+ #   t+=1
+  #  a=float(input("Введите число: "))
+   # if a==round(a): q+=1
+# print("Количество целых:", q)
+
+
+#1 zadanie
+#t=0 #кол-во чисел
+#q=0 #кол-во int чисел
+#for t in range(15):
+#    t+=1
+#    a=float(input("Введите число: "))
+#    if a==round(a): q+=1
+# print("Количество целых:", q)
+
+#2 zadanie
+#a=int(input("Введите число:"))
+#for a in range(a):
+#    a+=a
+#print(f"Сумма {a}")
+
+
+#3zadanie
+#from random import *
+#kt=1
+
+#for t in range(8):
+#    B=randint(-100,100)
+#    print(B)
+#    if B>0: kt*=B
+#print(f"Произведение чисел = ", kt)
+
+#4 zadanie
+#for a in range(10,20):
+#    print(a**2)
+
+#5 zadanie   -
+#from random import *
+#kt=1
+#A=randint(1,10)
+#for A in range(A):
+#    int(input("Введите число:"))
+#    if A<0: kt*=A
+#print("Отрицательные числа", kt)
+
+#6zadanie
+#from random import *
+#min=0
+#plus=0
+#zero=0
+#a=randint(3,10)
+#for b in range(a):
+#    q=int(input("Введите число: "))
+#    if q<0:
+#        min+=1
+#    elif q>0:
+#        plus+=1
+#    elif q==0:
+#        zero+=1
+#print(f"Кол-во отрицательных, {min}, Кол-во положительных {plus}, Кол-во нулей {zero}")
+
+#7zadanie
+#K=int(input("Введите число K:"))
+#A=int(input("Введите число A:"))
+#B=int(input("Введите число B:"))
+#for t in range(A,B)
+#if K%A==0:
+#    print(A)
+
+
+
+a=input("Купи слона! ")
+while a.lower() !="слон":
+    a=input(f"Все говорят {a}, а ты купи!")
+print("Слон твой")
+
+
+
+
+
+
+
+
